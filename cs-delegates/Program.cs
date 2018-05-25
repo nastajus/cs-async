31ef496d66ac7389d8131faa4d2e817bf059eb36
= <=>
2018-05-25 14:15:23 -0400

    delegate example basics

---
+++ cs-delegates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_delegates
+{
+    class Program
+    {
+        delegate string MyDel();
+
+        static void Main(string[] args)
+        {
+            MyDel del = new MyDel(TestFunction); // outdated, only C# 1.x era
+
+            MyDel dele = TestFunction; //better shorthand since C# 2.0
+
+            del += TestFunction2;
+            Console.WriteLine(del); //only has value returned by last method in invocation list of entire delegate.
+
+        }
+
+        static string TestFunction()
+        {
+            return "Hi";
+        }
+
+        static string TestFunction2()
+        {
+            return "Bye";
+        }
+
+    }
+
+}
--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 ###  Asynchronous Programming using C\#
 
 [Asynchronous Programming using C#: Lesson 1 - What is a thread?
-](https://www.youtube.com/watch?v=mkkcqQlIpeQ)
+](https://www.youtube.com/watch?v=mkkcqQlIpeQ) -- paused at 17:50
+
+[Advanced C#: Lesson 4 - Delegates Events and Lambda expressions](https://www.youtube.com/watch?v=8e2GEFNctwQ) -- at 14:00ish.
 
 [Asynchronous Programming using C#: Lesson 2 - Intro to Tasks](https://www.youtube.com/watch?v=IONqMWGn9-w)
-
 
