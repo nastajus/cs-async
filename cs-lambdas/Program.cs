7acab9881eb9559185ff73b0d63a8d557af987e4
= <=>
2018-05-25 15:02:23 -0400

    anonymous method with returns and parameters

--- cs-lambdas/Program.cs
+++ cs-lambdas/Program.cs
@@ -8,20 +8,24 @@ namespace cs_lambdas
 {
     class Program
     {
-        delegate void CountIt();
+        delegate int CountIt(int x);
 
         static void Main(string[] args)
         {
             //anonymous method
-            CountIt testDel = delegate
+            CountIt testDel = delegate(int y)
             {
+                int total = 0;
                 for (int i = 0; i < 5; i++)
                 {
-                    Console.WriteLine(i);
+                    total += y;
+                    //Console.WriteLine(y);
                 }
+                return total;
 
             };
-            testDel();
+            int overallTotal = testDel(100);
+            Console.WriteLine(overallTotal);
             //benefit: can only be used here, better as in-line.
 
 
