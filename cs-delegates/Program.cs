5c678b4f67b165a4f33c34fcaf34d8baf8787949
= <=>
2018-05-25 14:40:57 -0400

    delegate covariance and contravariance

--- cs-delegates/Program.cs
+++ cs-delegates/Program.cs
@@ -10,14 +10,23 @@ namespace cs_delegates
     {
         delegate string MyDel();
 
+        delegate Animal MyDeleg();
+
+        delegate void MyDelegate(Dog d);
+
+
         static void Main(string[] args)
         {
             MyDel del = new MyDel(TestFunction); // outdated, only C# 1.x era
 
             MyDel dele = TestFunction; //better shorthand since C# 2.0
 
-            del += TestFunction2;
-            Console.WriteLine(del); //only has value returned by last method in invocation list of entire delegate.
+            dele += TestFunction2;
+            Console.WriteLine(dele); //only has value returned by last method in invocation list of entire delegate.
+
+            MyDeleg deleg = TestFunctionDog; //covariance
+
+            MyDelegate delegi = TestFunctionAnimal; //contravariance
 
         }
 
@@ -30,7 +39,24 @@ namespace cs_delegates
         {
             return "Bye";
         }
+        static Dog TestFunctionDog()
+        {
+            return new Dog();
+        }
+        static void TestFunctionAnimal(Animal a)
+        {
+
+        }
 
     }
 
+    
+    class Animal
+    {
+
+    }
+    class Dog : Animal
+    {
+
+    }
 }
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 [Asynchronous Programming using C#: Lesson 1 - What is a thread?
 ](https://www.youtube.com/watch?v=mkkcqQlIpeQ) -- paused at 17:50
 
-[Advanced C#: Lesson 4 - Delegates Events and Lambda expressions](https://www.youtube.com/watch?v=8e2GEFNctwQ) -- at 14:00ish.
+[Advanced C#: Lesson 4 - Delegates Events and Lambda expressions](https://www.youtube.com/watch?v=8e2GEFNctwQ)
 
 [Asynchronous Programming using C#: Lesson 2 - Intro to Tasks](https://www.youtube.com/watch?v=IONqMWGn9-w)
 
